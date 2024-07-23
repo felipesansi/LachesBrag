064fb290a45ce2247a40e2c04311ac69aead07ca
Felipe Freitas dos Santos Silva <82713076+felipesansi@users.noreply.github.com>
2024-07-23 16:24:51 -0300

    migration popular categorias

---
+++ LachesBrag/Migrations/20240723165732_PopulandoCategorias.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace LachesBrag.Migrations
+{
+    public partial class PopulandoCategorias : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.Sql("insert into Categorias (CategoriaNome, Descricao) values ('Normal', 'Lanches com ingredientes nomais')");
+
+            migrationBuilder.Sql("insert into Categorias (CategoriaNome, Descricao) values ('Naturais', 'Lanches com ingredientes naturais')");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.Sql("DELETE FROM Categorias");
+        }
+    }
+}
