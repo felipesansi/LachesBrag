eeb09b20f7fef1e8c69a220e6509ce6b8fa9730a
Felipe Freitas dos Santos Silva <82713076+felipesansi@users.noreply.github.com>
2024-08-15 15:48:10 -0300

    Page Contato criada com TagHelper

---
+++ LachesBrag/TagHelpers/EmailTagHelpers.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.AspNetCore.Razor.TagHelpers;
+
+namespace LachesBrag.TagHelpers
+{
+    [HtmlTargetElement("email")]
+    public class EmailTagHelper : TagHelper
+    {
+        public string Endereco { get; set; }
+        public string Conteudo { get; set; }
+
+        public override void Process(TagHelperContext context, TagHelperOutput output)
+        {
+            // Define o nome da tag HTML para "a"
+            output.TagName = "a";
+
+            // Constrói o atributo "href" com o prefixo "mailto:"
+            var mailtoLink = $"mailto:{Endereco}";
+            output.Attributes.SetAttribute("href", mailtoLink);
+
+            // Define o conteúdo da tag "a"
+            output.Content.SetContent(Conteudo);
+        }
+    }
+}
