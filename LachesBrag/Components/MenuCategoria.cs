21462707feb38d1a1b466a95f935094f5710828e
Felipe Freitas dos Santos Silva <82713076+felipesansi@users.noreply.github.com>
2024-08-20 15:56:57 -0300

    MenuCategoria view Component feita

---
+++ LachesBrag/Components/MenuCategoria.cs
@@ -0,0 +1,19 @@
+﻿using LachesBrag.Repositories.Interfaces;
+using Microsoft.AspNetCore.Mvc;
+
+namespace LachesBrag.Components
+{
+    public class MenuCategoria : ViewComponent
+    {
+        private readonly ICategoriaRepository categoriaRepository;
+        public MenuCategoria(ICategoriaRepository categoriaRepository)
+        {
+            this.categoriaRepository = categoriaRepository;
+        }
+        public IViewComponentResult Invoke()
+        {
+            var categorias = categoriaRepository.Categorias.OrderBy(cat => cat.CategoriaNome);
+            return View(categorias);
+        }
+    }
+}
