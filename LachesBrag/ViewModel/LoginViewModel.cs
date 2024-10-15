344756e7fcc5cea11e17d210951fe198c49bbf35
Felipe Freitas <felipeprofissional22@gmail.com>
2024-10-15 16:13:41 -0300

    LoginViewModel criada

---
+++ LachesBrag/ViewModel/LoginViewModel.cs
@@ -0,0 +1,16 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace LachesBrag.ViewModel
+{
+    public class LoginViewModel
+    {
+        [Required(ErrorMessage ="Informe o nome de usuário") ]
+        [Display (Name ="Usuario")] 
+        public string UserName { get; set; }
+        [Required(ErrorMessage = "Informe o nome de usuário")]
+        [DataType (DataType.Password)]
+        [Display(Name = "Senha")]
+        public string Password { get; set; }
+        public string ReturnUrl { get; set; }
+    }
+}
